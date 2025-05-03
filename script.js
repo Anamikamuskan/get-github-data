d791baad9be4163897fea7b737d6a0af76a20a2c
Anamikamukku <anamikamuskan82@gmail.com>
2025-05-04 01:06:43 +0530

    changed ui

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
-const button = document.querySelector("#submit")  
-button.addEventListener("click",(e)=>{
-    const username = document.querySelector("#username").value
-    console.log(username)
-     
-    fetch(`https://api.github.com/users/${username}`)
-    .then(raw => raw.json())
-    .then(data =>{
-        document.getElementById('userData').innerHTML = `
-            <img src="${data.avatar_url}" alt="Avatar">
-            <h2>${data.name || 'No name provided'}</h2>
-            <p><strong>Username:</strong> ${data.login}</p>
-            <p><strong>Public Repos:</strong> ${data.public_repos}</p>
-            <p><strong>Followers:</strong> ${data.followers}</p>
-            <p><strong>Following:</strong> ${data.following}</p>
-            <p><a href="${data.html_url}" target="_blank">View Profile</a></p>
-          `;
-      
+const submitBtn = document.querySelector(".submit");
+let username = document.querySelector(".username")
+
+
+  function getUserData(username){
+     return fetch(`https://api.github.com/users/${username}`)
+    .then(raw => {
+        if(!raw.ok) {
+            throw new Error(' Please Enter correct username ');  // This will trigger catch
+        }
+        console.log(raw)
+        return raw.json()
     })
+  }
+
+  function fillUserData(data){
+        return  `<img src="${data.avatar_url}" alt="Profile Picture"
+                 class="w-28 h-28 rounded-full mx-auto shadow-lg mb-4 object-cover">
+            <h2 class="text-xl font-semibold text-gray-800 mb-4">${data.name ?? data.login}</h2>
+
+            <div class="flex justify-around text-center text-gray-600 mb-4">
+              <div>
+                <span class="block text-lg font-bold text-indigo-500">${data.followers}</span>
+                Followers
+              </div>
+              <div>
+                <span class="block text-lg font-bold text-indigo-500">${data.following}</span>
+                Following
+              </div>
+              <div>
+                <span class="block text-lg font-bold text-indigo-500">${data.public_repos}</span>
+                Repos
+              </div>
+            </div>
+        `;
+  }
+
+  
+  submitBtn.addEventListener("click",(e)=>{
+    e.preventDefault()
+    const usernameValue = username.value.trim();
+
+    console.log(usernameValue)
+    getUserData(usernameValue) 
+    .then(data =>{
+        console.log(data)
+        document.querySelector('.profile-card').innerHTML = fillUserData(data)
+      
+    }).catch(()=>{
+        document.querySelector('.profile-card').innerHTML = `
+        <p style="color:red"> Please enter correct username!!! </p>`
+    })
+
 
 })
